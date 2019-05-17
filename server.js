5147b45fe96ab9df947eb9699311d973e915c692
Mark Carlson <mark@markcarlson.io>
2019-05-16 17:53:46 -0700

    updates package.json and removes body-parser

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
